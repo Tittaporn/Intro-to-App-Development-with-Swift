7a7dd0310592431750e47d94892cf56238ecbe30
Lee McCormick <57606580+Tittaporn@users.noreply.github.com>
2021-01-01 00:28:02 -0600

    Add files via upload
    Lesson 1-5 Swift Exercises from Intro to App Development with Swift.

---
+++ 2 Naming and Indentifiers/Naming.playground/Pages/13-ExerciseNaming.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+
+//: - experiment: Rewrite the code so that it makes sense without needing a note. Use meaningful names and comments.
+let numberOfOragesPallets = 14
+let numberOfWatermelonsPallets = 3
+let orageWeightPerPallets = 100
+let watermelonWeightPerPallets = 200
+let totalOrageWeight = numberOfOragesPallets * orageWeightPerPallets
+let totalWatermelonWeight = numberOfWatermelonsPallets * watermelonWeightPerPallets
+let totalWeight = totalOrageWeight + totalWatermelonWeight
+let halfOfTotalWeight = totalWeight / 2
+let amountOfOrangeForHalfweight = halfOfTotalWeight / orageWeightPerPallets
+let numberOfOragesPalletsForhalfWeightInTruck = numberOfOragesPallets - amountOfOrangeForHalfweight
+
+
+
+
+
+/*:
+ 
+ _Copyright © 2017 Apple Inc._
+ 
+ _Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:_
+ 
+ _The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software._
+ 
+ _THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE._
+ */
+//:[Previous](@previous)  |  page 14 of 14
